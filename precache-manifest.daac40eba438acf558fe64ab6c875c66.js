fa72a27d60a5fe5dda2b8f92686d71cbe5302d90
ejaramillom <ejaramilo@hotmail.com>
2020-05-15 16:53:28 -0500

    Updates

---
+++ precache-manifest.daac40eba438acf558fe64ab6c875c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5de3e4080f5a31f198d2b7391b45fa",
+    "url": "/post-react/index.html"
+  },
+  {
+    "revision": "23b95c2e9cb437f11724",
+    "url": "/post-react/static/css/main.138470cb.chunk.css"
+  },
+  {
+    "revision": "3f2eba8696be036e5228",
+    "url": "/post-react/static/js/2.501479fc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/post-react/static/js/2.501479fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b95c2e9cb437f11724",
+    "url": "/post-react/static/js/main.86a19ab6.chunk.js"
+  },
+  {
+    "revision": "0474ed1e87f0f736d71e",
+    "url": "/post-react/static/js/runtime-main.6efcab54.js"
+  }
+]);
